650c6acba7e46b783f92ac26a8660643b956637e
Alex Nadzeika <nadeykoa@gmail.com>
2021-01-07 16:03:43 +0100

    Added functionality for saving and editing (one filter).

--- web_part.py
+++ web_part.py
@@ -36,8 +36,11 @@
 import os
 import random
 import string
+import cv2 as cv
+import numpy as np
 
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, send_file
 
 app = Flask(__name__)
 app.config['UPLOAD_PATH'] = 'uploads'
@@ -74,6 +77,25 @@ def upload_image(image_name):
 @app.route('/upload/<filename>')
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_file(os.path.join(app.config['UPLOAD_PATH'], filename), as_attachment=True, attachment_filename=filename)
+
+@app.route('/main_page/<filename>/<mode>', methods=["POST"])
+def operation(filename, mode):
+
+    image = cv.imread(os.path.join(app.config['UPLOAD_PATH'], filename))
+    hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+    file_extension = filename.split('.')[1]
+    # new_image_name = generate_imagename(file_extension)
+    edited_image_name = generate_imagename(file_extension)
+    cv.imwrite(os.path.join(app.config['UPLOAD_PATH'], edited_image_name), hsv)
+
+
+    return redirect(url_for('main_page', image_name=edited_image_name))
+
 
 def generate_imagename(extension):
     number_of_characters = 7
